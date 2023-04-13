716a72bf3efa516ebce048e2748212f86620e2a0
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-13 08:36:43 -0300

    add script e btn functions

---
+++ script.js
@@ -0,0 +1,10 @@
+var textArea = document.querySelector('#textarea');
+let btnEncrypt = document.querySelector('#btn-encrypt');
+let btnDecrypt = document.querySelector('#btn-decrypt');
+
+function encrypt() {
+  alert(textArea.value);
+}
+function decrypt() {
+  // alert(textArea.value);
+}
