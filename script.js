292e7a5768acf8855b6f6fd7441a6eef1796726e
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-13 09:14:31 -0300

    incrementa encrypt function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var textArea = document.querySelector('#textarea');
+var textArea = document.querySelector('#textarea').value;
 let btnEncrypt = document.querySelector('#btn-encrypt');
 let btnDecrypt = document.querySelector('#btn-decrypt');
+let noResult = document.querySelector('#no-result');
+let resultText = document.querySelector('#result__text');
 
 function encrypt() {
-  alert(textArea.value);
+  noResult.style.display = 'none';
+  resultText.style.display = 'block';
+  // resultText.innerHTML = '<p>' + textArea + '<p>';
 }
 function decrypt() {
   // alert(textArea.value);
