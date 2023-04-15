e39394292205e82a928c778ea0babb32c8a8033a
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-15 16:18:07 -0300

    add logica de sumir e aparecer nos botoes

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var textArea = document.querySelector('#textarea').value;
-let btnEncrypt = document.querySelector('#btn-encrypt');
-let btnDecrypt = document.querySelector('#btn-decrypt');
-let noResult = document.querySelector('#no-result');
-let resultText = document.querySelector('#result__text');
+var textArea = document.querySelector('#textarea');
+var btnEncrypt = document.querySelector('#btn-encrypt');
+var btnDecrypt = document.querySelector('#btn-decrypt');
+var btnCopy = document.querySelector('#btn-copy');
+var noResult = document.querySelector('#no-result');
+var resultDiv = document.querySelector('#result');
+var resultText = document.querySelector('#result__text');
+
+btnEncrypt.onclick = encrypt;
+// btnDecrypt.onclick = decrypt;
 
 function encrypt() {
-  noResult.style.display = 'none';
-  resultText.style.display = 'block';
-  // resultText.innerHTML = '<p>' + textArea + '<p>';
+  if (textArea.value === ' ') {
+    alert('Digite uma palavra');
+  } else {
+    // tira img do card resultado
+    noResult.style.display = 'none';
+
+    // coloca o texto inserido no campo do resultado
+    resultText.innerHTML = textArea.value;
+
+    //faz a div resultado e o botão aparecer
+    resultDiv.style.display = 'block';
+    resultText.style.display = 'block';
+    btnCopy.style.display = 'block';
+  }
 }
 function decrypt() {
   // alert(textArea.value);
