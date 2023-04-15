8a55483f8d75961c19778b6f5f9baee5f56cceff
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-15 17:35:58 -0300

    testa nova soluçao

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var textArea = document.querySelector('#textarea');
+var frase = document.getElementById('textarea').value.toLowerCase;
 var btnEncrypt = document.querySelector('#btn-encrypt');
 var btnDecrypt = document.querySelector('#btn-decrypt');
 var btnCopy = document.querySelector('#btn-copy');
@@ -9,15 +9,41 @@ var resultText = document.querySelector('#result__text');
 btnEncrypt.onclick = encrypt;
 // btnDecrypt.onclick = decrypt;
 
-function encrypt() {
-  if (textArea.value === ' ') {
+function analisaTexto(texto) {
+  let encryptedText = '';
+
+  for (let i = 0; i < texto.length; i++) {
+    switch (texto[1]) {
+      case 'a':
+        encryptedText += 'ai';
+        break;
+    }
+  }
+
+  return encryptedText;
+}
+
+//-----------------------TEST
+function encripta() {
+  var textoEncriptado = frase.replace(/e/gim, 'enter');
+  var textoEncriptado = frase.replace(/i/gim, 'imes');
+  var textoEncriptado = frase.replace(/a/gim, 'ai');
+  var textoEncriptado = frase.replace(/o/gim, 'ober');
+  var textoEncriptado = frase.replace(/u/gim, 'ufat');
+}
+//-----------------------TEST
+
+function encrypt(encryptedText) {
+  if (frase.value === ' ') {
     alert('Digite uma palavra');
   } else {
+    analisaTexto(frase.value);
+    console.log(encryptedText);
     // tira img do card resultado
     noResult.style.display = 'none';
 
     // coloca o texto inserido no campo do resultado
-    resultText.innerHTML = textArea.value;
+    resultText.innerHTML = frase.value;
 
     //faz a div resultado e o botão aparecer
     resultDiv.style.textAlign = 'start';
@@ -25,6 +51,7 @@ function encrypt() {
     btnCopy.style.display = 'block';
   }
 }
+
 function decrypt() {
-  // alert(textArea.value);
+  // alert(frase.value);
 }
